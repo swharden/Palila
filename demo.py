1fdef89857ce6e7954ff2d96ef7f6e29d4ec5b71
Scott W Harden <swharden@gmail.com>
2021-06-09 20:28:08 -0400

    simplify build script

--- demo.py
+++ demo.py
@@ -10,6 +10,10 @@ sys.path.insert(0, str(PATH_REPO.joinpath("src")))
 import palila
 
 if __name__ == "__main__":
+    
+    demoFolder = PATH_REPO.joinpath("demo")
+    palila.recursivelyDeleteIndexes(demoFolder)
+    
     templateFile = PATH_REPO.joinpath("demo/template.html")
     demoFolder = PATH_REPO.joinpath("demo")
     palila.recursivelyMakeIndexes(demoFolder, templateFile)
