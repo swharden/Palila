433af6ae1781f24faabd560188e725b9de1c049b
Scott W Harden <swharden@gmail.com>
2021-06-13 16:37:09 -0400

    automatic named headings

--- palila.py
+++ palila.py
@@ -16,7 +16,8 @@ def recursivelyMakeIndexes(folder: pathlib.Path, templateFile: pathlib.Path):
             print(f"deleting\t{indexFile.resolve()}")
             indexFile.unlink()
         print(f"generating\t{indexFile}")
-        makeIndex(indexMarkdownFile, templateFile)
+        pg = PageGenerator(indexMarkdownFile, templateFile)
+        pg.save()
     for subFolder in [x for x in folder.glob("**/*") if x.is_dir()]:
         recursivelyMakeIndexes(subFolder, templateFile)
 
@@ -40,100 +41,134 @@ def safeUrl(s: str):
     # disallow multiple dashes
     while "--" in tag:
         tag = tag.replace("--", "-")
-    
+
     # disallow starting or ending with a dash
     tag = tag.strip("-")
-    
+
     return tag
 
 
-def makeIndex(markdownFile: pathlib.Path, templateFile: pathlib.Path):
-    """
-    Convert the given index.md to index.html using the template
-    """
-    if markdownFile.exists() == False:
-        raise Exception("does not exist: " + markdownFile.resolve())
-    if (markdownFile.name != "index.md"):
-        raise Exception("markdown files must be named index.md")
-    if templateFile.exists() == False:
-        raise Exception("does not exist: " + markdownFile.resolve())
-    timeStart = time.time()
+class PageGenerator:
 
-    # read markdown
-    markdownLines = markdownFile.read_text(encoding="utf-8").split("\n")
+    def __init__(self, markdownFile: pathlib.Path, templateFile: pathlib.Path):
+        self.folder = markdownFile.parent
+        self.timeStart = time.time()
 
-    # make anchors clickable and remember TOC items
-    insideCodeBlock = False
-    tocItems = []
-    for i, line in enumerate(markdownLines):
-        if (line.startswith("```")):
-            insideCodeBlock = ~insideCodeBlock
-        if (insideCodeBlock):
-            continue
-        if "# [" in line:
-            continue
-        if (line.startswith("#")):
-            hashes, title = line.split(" ", 1)
-            markdownLines[i] = f"{hashes} [{title}](#{safeUrl(title)})"
-            tocItems.append([len(hashes), title])
+        # validate paths are legit
+        if markdownFile.exists() == False:
+            raise Exception("does not exist: " + markdownFile.resolve())
+        if (markdownFile.name != "index.md"):
+            raise Exception("markdown files must be named index.md")
+        if templateFile.exists() == False:
+            raise Exception("does not exist: " + markdownFile.resolve())
 
-    # replace one-line links with special content
-    insideCodeBlock = False
-    for i, line in enumerate(markdownLines):
-        if (line.startswith("```")):
-            insideCodeBlock = ~insideCodeBlock
-        if (insideCodeBlock):
-            continue
-        isLinkLine = line.startswith("![](") and line.strip().endswith(")")
-        if not isLinkLine:
-            continue
+        # break markdown into lines and identify which are code
+        markdownLines = markdownFile.read_text(encoding="utf-8").split("\n")
+        linesWithoutCode = self.getMarkdownLinesWithoutCode(markdownLines)
 
-        url = line[4:-1]
-        extension = url.lower().split(".")[-1]
+        # make anchors clickable and remember TOC items
+        tocItems = []
+        for i in linesWithoutCode:
+            line = markdownLines[i]
+            if "# [" in line:
+                continue
+            if (line.startswith("#")):
+                hashes, title = line.split(" ", 1)
+                markdownLines[i] = f"{hashes} [{title}](#{safeUrl(title)})"
+                tocItems.append([len(hashes), title])
 
-        if extension == "png" or extension == "jpg" or extension == "gif":
-            markdownLines[i] = f"<a href='{url}'><img src='{url}'></a>"
-            continue
+        # replace one-line links with special content
+        for i in linesWithoutCode:
+            line = markdownLines[i]
+            isLinkLine = line.startswith("![](") and line.strip().endswith(")")
+            if not isLinkLine:
+                continue
 
-        if "://youtu" in line.lower():
-            url = "https://www.youtube.com/embed/" + pathlib.Path(url).name
-            markdownLines[i] = "<div class='ratio ratio-16x9 my-5'>" + \
-                f"<object class='border border-dark shadow' data='{url}'></object></div>"
-            continue
+            url = line[4:-1]
+            extension = url.lower().split(".")[-1]
 
-    # add TOC to page
-    insideCodeBlock = False
-    for i, line in enumerate(markdownLines):
-        if (line.startswith("```")):
-            insideCodeBlock = ~insideCodeBlock
-        if (insideCodeBlock):
-            continue
-        if line.startswith("![](TOC)"):
-            tocMarkdown = ""
-            for level, title in tocItems:
-                tocMarkdown += "  " * level + \
-                    f"* [{title}](#{safeUrl(title)})\n"
-            markdownLines[i] = tocMarkdown
+            if extension == "png" or extension == "jpg" or extension == "gif":
+                markdownLines[i] = f"<a href='{url}'><img src='{url}'></a>"
+                continue
 
-    # https://github.com/Python-Markdown/markdown/wiki/Third-Party-Extensions
-    markdownHtml = markdown.markdown("\n".join(markdownLines),
-                                     extensions=['fenced_code', 'tables', 'meta', 'md_in_html'])
+            if "://youtu" in line.lower():
+                url = "https://www.youtube.com/embed/" + pathlib.Path(url).name
+                markdownLines[i] = "<div class='ratio ratio-16x9 my-5'>" + \
+                    f"<object class='border border-dark shadow' data='{url}'></object></div>"
+                continue
 
-    # read template and perform replacements
-    html = templateFile.read_text()
-    defaultReplacements = {
-        "{{HEAD_TITLE}}": markdownFile.parent.name,
-        "{{BUILD_UTC_DATE}}": str(datetime.datetime.utcnow().strftime("%x")),
-        "{{BUILD_UTC_TIME}}": str(datetime.datetime.utcnow().strftime("%X")),
-        "{{BUILD_TIME_MS}}": f"{(time.time() - timeStart) * 1000:.1f}",
-        "{{CONTENT}}": markdownHtml,
-    }
-    for key, value in defaultReplacements.items():
-        html = html.replace(key, value)
+        # add TOC to page
+        for i in linesWithoutCode:
+            if markdownLines[i].startswith("![](TOC)"):
+                tocMarkdown = ""
+                for level, title in tocItems:
+                    tocMarkdown += "  " * level + \
+                        f"* [{title}](#{safeUrl(title)})\n"
+                markdownLines[i] = tocMarkdown
 
-    # save output
-    outputFile = markdownFile.parent.joinpath("index.html")
-    outputFile.write_text(html, encoding="utf-8")
+        # convert markdown to HTML
+        # https://github.com/Python-Markdown/markdown/wiki/Third-Party-Extensions
+        htmlLines = markdown.markdown("\n".join(markdownLines),
+                                      extensions=['fenced_code', 'tables', 'meta', 'md_in_html']).split("\n")
+
+        # add anchors to headings
+        for i in self.getHtmlLinesWithoutCode(htmlLines):
+            line = htmlLines[i]
+            if line.startswith("<h") and "href" in line:
+                anchorName = line.split('"')[1][1:]
+                htmlLines[i] = line.replace(">", f" id='{anchorName}'>", 1)
+
+        # read template and perform replacements
+        html = templateFile.read_text()
+        defaultReplacements = {
+            "{{HEAD_TITLE}}": markdownFile.parent.name,
+            "{{BUILD_UTC_DATE}}": str(datetime.datetime.utcnow().strftime("%x")),
+            "{{BUILD_UTC_TIME}}": str(datetime.datetime.utcnow().strftime("%X")),
+            "{{BUILD_TIME_MS}}": f"{(time.time() - self.timeStart) * 1000:.1f}",
+            "{{CONTENT}}": "\n".join(htmlLines),
+        }
+        for key, value in defaultReplacements.items():
+            html = html.replace(key, value)
+
+        self.html = html
+
+    def save(self, filename="index.html", deleteExisting=True):
+        """
+        Save the generated HTML as a file
+        """
+        outputFile = self.folder.joinpath(filename)
+        if deleteExisting and outputFile.exists():
+            outputFile.unlink()
+        outputFile.write_text(self.html, encoding="utf-8")
+
+    def getMarkdownLinesWithoutCode(self, markdownLines: list[str]) -> list[bool]:
+        linesWithoutCode = []
+        insideCodeBlock = False
+        for i, line in enumerate(markdownLines):
+            if (line.startswith("```")):
+                insideCodeBlock = ~insideCodeBlock
+            if (insideCodeBlock):
+                continue
+            else:
+                linesWithoutCode.append(i)
+        return linesWithoutCode
+
+    def getHtmlLinesWithoutCode(self, htmlLines: list[str]) -> list[bool]:
+        linesWithoutCode = []
+        insideCodeBlock = False
+        for i, line in enumerate(htmlLines):
+            if "<pre>" in line:
+                insideCodeBlock = True
+                continue
+            if "</pre>" in line:
+                insideCodeBlock = False
+                continue
+            if not insideCodeBlock:
+                linesWithoutCode.append(i)
+        return linesWithoutCode
+
+    def __repr__(self) -> str:
+        return f"page generator for {self.folder}"
 
 
 if __name__ == "__main__":
